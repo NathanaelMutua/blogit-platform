2576af25a8b37acdea5d245610e39f758673b24e
nxthxnael <nathanael.mutua.m@gmail.com>
2025-07-08 13:07:52 +0300

    feat: add post endpoint to register user - install bcryptjs - import prisma client and bcrypt in index.ts to handle password hashing and database - add POST '/auth/register' endpoint to handle user registration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,40 @@
 import express, { Express } from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const app: Express = express();
+const myClient = new PrismaClient();
+
+app.use(express.json());
 
 app.get("/", (_req, res) => {
   res.send(`<h1>Blog Platform</h1>`);
 });
 
-const port = process.env.PORT || 5500;
+app.post("/auth/register", async (req, res) => {
+  try {
+    const { firstName, lastName, email, username, password, profileImage } =
+      req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await myClient.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        username,
+        password: hashedPassword,
+        profileImage,
+      },
+    });
+    res.status(201).json({ message: "User registered successfully!" });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "An error occurred!" });
+  }
+});
+
+const port = process.env.PORT || 5800;
 
 app.listen(port, () => console.log(`App is running on port ${port}`));
