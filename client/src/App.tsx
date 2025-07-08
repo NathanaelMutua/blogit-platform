664da01ff782fc8c844a8df052cb1c67a06ffc31
nxthxnael <nathanael.mutua.m@gmail.com>
2025-07-08 11:58:09 +0300

    feat: add basic registration form - install react-query - install material UI - clear default styling in App.css - delete index.css and its import in main.tsx - clear default README.md and add basic description - add basic form structure to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Button, Card, TextField, Typography } from "@mui/material";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box width="600px" margin="auto" p="2rem">
+        <Typography variant="h2" fontWeight="bold" fontSize="2rem">
+          Registration Form
+        </Typography>
+        <Card
+          sx={{
+            padding: "2rem 3rem",
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            alignItems: "center",
+          }}
+        >
+          <TextField label="first name" fullWidth size="small"></TextField>
+          <TextField label="last name" fullWidth size="small"></TextField>
+          <TextField label="username" fullWidth size="small"></TextField>
+          <TextField
+            label="email"
+            type="email"
+            fullWidth
+            size="small"
+          ></TextField>
+          <TextField
+            label="password"
+            type="password"
+            fullWidth
+            size="small"
+          ></TextField>
+          <TextField label="image url" fullWidth size="small"></TextField>
+          <Button variant="contained" fullWidth>
+            Register
+          </Button>
+        </Card>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
