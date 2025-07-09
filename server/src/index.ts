bceadaba2be2230281b17d61e263a0a09f73cdb3
nxthxnael <nathanael.mutua.m@gmail.com>
2025-07-09 15:06:06 +0300

    feat: add validation using zod for user registration - install zod - add validations to schema - parse the data from the request body to validate the data entered - add simple error handling to display message string in ZodError error

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,32 @@
 import express, { Express } from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
+import * as z from "zod";
 
 const app: Express = express();
 const myClient = new PrismaClient();
 
 app.use(express.json());
+
+// using zod for validation
+const userSchema = z.object({
+  firstName: z
+    .string({ required_error: "First Name is required" })
+    .refine((val) => val.length > 2, {
+      message: "First Name must be more than 2 letters",
+    }),
+  lastName: z
+    .string({ required_error: "Last Name is required" })
+    .refine((val) => val.length > 0, {
+      message: "Last Name is Required",
+    }),
+  email: z.string({ required_error: "Email is Required" }).email(),
+  username: z.string(),
+  password: z.string().refine((val) => val.length > 7, {
+    message: "Password is must be at least 8 characters, try again!",
+  }),
+  profileImage: z.string().url().optional(),
+});
 
 app.get("/", (_req, res) => {
   res.send(`<h1>Blog Platform</h1>`);
@@ -13,25 +34,46 @@ app.get("/", (_req, res) => {
 
 app.post("/auth/register", async (req, res) => {
   try {
-    const { firstName, lastName, email, username, password, profileImage } =
-      req.body;
+    const parsed = userSchema.safeParse(req.body);
 
+    if (!parsed.success) {
+      console.log(parsed.error);
+
+      res
+        .status(400)
+        .json({ my_error: parsed.error.issues.map((issue) => issue.message) });
+      return;
+    }
+
+    const { firstName, lastName, email, username, password, profileImage } =
+      parsed.data;
+
+    const newUserName = await myClient.user.findFirst({
+      where: { username },
+    });
+
+    if (newUserName) {
+      res.status(400).json({ message: "Username has already been used!" });
+      return;
+    }
+
+    // we add the hashed password after validation
     const hashedPassword = await bcrypt.hash(password, 10);
 
     await myClient.user.create({
       data: {
         firstName,
         lastName,
-        email,
         username,
+        email,
         password: hashedPassword,
         profileImage,
       },
     });
+
     res.status(201).json({ message: "User registered successfully!" });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ message: "An error occurred!" });
+  } catch {
+    res.status(500).json({ error: "An Error Occurred!" });
   }
 });
 
