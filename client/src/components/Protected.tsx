e8926e84b03c1efe62043288a1a65ba9515defb1
nxthxnael <nathanael.mutua.m@gmail.com>
2025-07-12 13:31:59 +0300

    feat: add protected component - add Protected.tsx component to implement protected routes to logged in users only - import use Effect and set it that on every render, a protected page checks if there are user details generated during login then if so allows the display of the protected children elements

---
+++ client/src/components/Protected.tsx
@@ -0,0 +1,19 @@
+import type React from "react";
+import useUser from "../store/userStore";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+function Protected({ children }: { children: React.ReactNode }) {
+  const { user } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user]);
+
+  return <>{children}</>;
+}
+
+export default Protected;
