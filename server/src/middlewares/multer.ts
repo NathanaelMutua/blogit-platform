a32736e5e6dab46d6617e207ebe3aa6945f571a2
nxthxnael <nathanael.mutua.m@gmail.com>
2025-07-13 03:46:37 +0300

    feat: add doten to cloudinary config - add dotenv to get environment variables from .env file - add and comment out other implementation of multer upload middleware using disk storage

--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -5,3 +5,16 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 export default upload;
+
+// Using disk storage instead of memory:
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//     filename: function(_req,file,cb){
+//         cb(null, file.originalname)
+//     }
+// })
+
+// const upload = multer({storage: storage})
+
+// export default upload;
