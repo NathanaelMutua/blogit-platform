e4fbd3e54c5a5fc30faa0de0967cb3ed2afce5c0
nxthxnael <nathanael.mutua.m@gmail.com>
2025-07-15 13:01:32 +0300

    chore:(index.ts) update origin URL to deployed frontend site URL

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://blogit-the-writers-platform.vercel.app"],
     methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
     credentials: true, // Allow cookies to be sent
   })
