439832428331cfdee82dc4f67acc34825d7d5206
nxthxnael <nathanael.mutua.m@gmail.com>
2025-07-15 14:19:59 +0300

    build: update axios instance baseURLto deployed database

--- client/src/api/axios.instance.ts
+++ client/src/api/axios.instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5800",
+  baseURL: "https://blogit-platform.onrender.com",
   withCredentials: true,
 });
 
